933a6fb4f57ce720e6a73cae8fdb28a05057ec00
Aazer <aazer_rae@yahoo.com>
2019-04-22 19:39:09 -0600

    Updated Messages

--- messages.cpp
+++ messages.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#inlcude "connectivityGraph.hpp"
+#include "connectivityGraph.hpp"
 using namespace std;
  //increment the vector of vector in connectivity graph like matched list and the vector of bools
 
  int main()
  {
-   connectivityGraph *ptrG1 = &g1; //obtains a variable that uses the current user's data
+   sting name = "Mark";
+   ConnectivityGraph g1;
+   PersonVertex *temp = g1.findVertex(g1.NameToId(name)); //obtains a variable that uses the current user's data
    vector<int> list; //list of matches with indexes of people matched with the current user
 
    //obtains the list of matched indexes
-   for(int i = 0; *ptrG1.vertices.size(); i++)
+   for(int i = 0; g1->getCurrentAmtOfNames(); i++)
    {
-     if(*ptrG1.swipedBack(*ptrG1.id, i))
+     if(g1.swipedBack(temp->id, i))
      {
        list.push_back(i); //gets indexes of people who's matched with
      }
@@ -22,7 +24,7 @@ using namespace std;
    //outputs matched list
    for(int i = 0; i < list.size();i++)
    {
-     cout << "You've matched with " << *ptrG1.IdToName(i) << endl;
+     cout << "You've matched with " << g1.IdToName(i) << endl;
    }
 
    //determines who is matched with who
@@ -30,9 +32,9 @@ using namespace std;
    cout << "Who would you like to talk to?" << endl;
    cout << "Or enter q to exit" << endl;
    getline(cin,input);
-   while(input != 'q')
+   while(input != "q")
    {
-     if(!(*ptrG1.isInGraph(input)) && *ptrG1.swipedBack(*ptrG1.id, *ptrG1.NameToId(input)))
+     if(!(g1.isInGraph(input)) && g1.swipedBack(temp->id, g1.NameToId(input)))
      {
        break;
      }
@@ -40,7 +42,7 @@ using namespace std;
      {
        cout << "That is not a person you are matched with. Please enter a different name or enter q to exit" << endl;
        getline(cin,input);
-       if(input == 'q')
+       if(input == "q")
        {
          break;
        }
@@ -48,22 +50,37 @@ using namespace std;
    }
 
    //messaging people
-   if(input != 'q') //if they entered a correct name they matched with
+   if(input != "q") //if they entered a correct name they matched with
    {
-     int index = *ptrG1.NameToId(input);
-     int choice;
-     while(choice != 4)
+     int index = g1.NameToId(input);
+     string choice;
+     while(stoi(choice) != 4)
      {
        cout << "Select an option: " << endl;
        cout << "1) Send a message to " << input << endl;
        cout << "2) See messages with " << input << endl;
        cout << "3) Unmatch with " << input << endl;
        cout << "4) Quit" << endl;
-       switch(choice)
+       getline(cin, choice);
+       switch(stoi(choice))
        {
          case 1:
          {
-           if()//they have talked yet to determine who has messaged first
+           string message;
+           //they have talked yet to determine who has messaged first
+           if(*ptrG1.messages.empty()) //if you have messaged messaged First
+           {
+             *ptrG1.messagedFirst.at(*ptrG1.NameToId(input)) = true;
+             cout << "Enter your message: " << endl;
+             getline(cin, message);
+             *ptrG1.messages[*ptrG1.NameToId(input)][0] = message;
+           }
+           else
+           {
+             cout << "Enter your message: " << endl;
+             getline(cin, message);
+             *ptrG1.messages[*ptrG1.NameToId(input)].push_back(message);
+           }
          }
          case 2:
          {
@@ -73,7 +90,25 @@ using namespace std;
            }
            else //there are messages between the user and targeted person
            {
-             if(*ptrG1.messagedFirst.at())
+             if(*ptrG1.messagedFirst.at(*ptrG1.NameToId(input))) //user send first message
+             {
+               for(int i = 0; i < *ptrG1.messages[*ptrG1.NameToId(input)].size(); i++)
+               {
+                 cout << "You: " << *ptrG1.messages[*ptrG1.NameToId(input)][i] << endl << endl;
+                 cout << input << ": ";
+                 cout << *ptrG1.messages[*ptrG1.NameToId(input)][i+1] << endl << endl;
+
+               }
+             }
+             else //targeted person has sent first message
+             {
+               for(int i = 0; i < *ptrG1.messages[*ptrG1.NameToId(input)].size(); i++)
+               {
+                 cout << input << ": ";
+                 cout << *ptrG1.messages[*ptrG1.NameToId(input)][i] << endl << endl;
+                 cout << "You: " << *ptrG1.messages[*ptrG1.NameToId(input)][i+1] << endl << endl;
+               }
+             }
            }
          }
          case 3:
